a6754c20f71dd9dacafee3f4a9c2d8c378362a91
Bruno, Furukawa <furukawaborgesbruno@gmail.com>
2019-07-05 17:06:54 -0300

    Adicionando logo twitch.tv

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const electron = require('electron')
+const path = require('path')
 const { app, BrowserWindow } = electron;
 
 app.on('ready', () => {
     const window = new BrowserWindow({
                         alwaysOnTop: true,
-                        frame: false,
+                        //frame: false,
+                        icon: path.join(__dirname, 'assets/images/icons/64x64.png')
                         // width: 250,
                         // height: 500
                     })
