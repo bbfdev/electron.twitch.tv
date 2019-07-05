bd0a1a4efb918ba8734422ad50195fb1241ea474
Bruno, Furukawa <furukawaborgesbruno@gmail.com>
2019-07-05 18:01:09 -0300

    ajustes no player e no tray

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const electron = require('electron')
 const path = require('path')
 const ChronoTray = require('./app/chronotray')
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcRenderer} = electron;
+const log = require('electron-log');
 
 let window;
 
 app.on('ready', () => {
     // cria icone na bara de tarefas
     tray = new ChronoTray(path.join(__dirname, 'assets/images/icons/16x16.png'));
+
     tray.on('click', (event, bounds, position) => {
         if (window.isVisible()) {
             window.hide()
@@ -15,19 +17,34 @@ app.on('ready', () => {
             window.show();
         }
     })
+
     //cria a janela
     window = new BrowserWindow({
                         alwaysOnTop: true,
                         frame: false,
                         icon: path.join(__dirname, 'assets/images/icons/64x64.png'),
-                        // width: 250,
-                        // height: 500,
+                        backgroundColor: '#000000',
+                        width: 620,
+                        height: 378,
                         show: false
                     })
+
     window.setMenu(null);
     window.loadURL(`file://${__dirname}/views/twitch.tv.html`);
 
     window.once('ready-to-show', () => {
+        log.info(`Aplicação iniciada`)
         window.show()
     })
 })
+
+app.on('window-all-closed', () => {
+    log.info(`Todas as janelas foram fechadas.`)
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+
