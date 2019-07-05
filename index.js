460ef243551348413d4d88418390ff3b297c5761
Bruno, Furukawa <furukawaborgesbruno@gmail.com>
2019-07-05 17:14:43 -0300

    Removendo opções de minimizar, maximizar e fechar, acionando a window somente quando estiver disponivel para aparecer

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ const electron = require('electron')
 const path = require('path')
 const { app, BrowserWindow } = electron;
 
+let window;
+
 app.on('ready', () => {
-    const window = new BrowserWindow({
+    window = new BrowserWindow({
                         alwaysOnTop: true,
-                        //frame: false,
-                        icon: path.join(__dirname, 'assets/images/icons/64x64.png')
+                        frame: false,
+                        icon: path.join(__dirname, 'assets/images/icons/64x64.png'),
                         // width: 250,
-                        // height: 500
+                        // height: 500,
+                        show: false
                     })
     window.setMenu(null);
     window.loadURL(`file://${__dirname}/views/twitch.tv.html`);
+
+    window.once('ready-to-show', () => {
+        window.show()
+    })
 })
